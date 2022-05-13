452cf0cae6bd0f35bde23153eabb4bc563997906
leephan <pttl2k1@gmail.com>
2022-05-13 19:37:22 +0700

    style: 💄 header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Header from '~/components/Header';
 
 const Home: NextPage = () => {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
             <Head>
-                <title>Create Next App</title>
+                <title>Home - Netflix</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            Let build Netflix
+
+            <Header />
+            <main>
+                {/* Banner  */}
+                <section>
+                    {/* row */}
+                    {/* row */}
+                    {/* row */}
+                    {/* row */}
+                    {/* row */}
+                    {/* row */}
+                    {/* row */}
+                </section>
+            </main>
         </div>
     );
 };
