78bf4a1d89cccaf2ed0cfe01f1f91aa1cc1bf9e2
leephan <pttl2k1@gmail.com>
2022-05-13 10:45:31 +0700

    config alias import

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 module.exports = {
     reactStrictMode: true,
+    webpack: (config) => {
+        config.resolve.alias['~'] = path.resolve(__dirname);
+        return config;
+    },
 };
