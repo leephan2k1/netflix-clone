f44001469f52a6e0a7a24e3eb156e5a12161ddd9
leephan <pttl2k1@gmail.com>
2022-05-13 23:47:11 +0700

    perf: ⚡️ change to local image

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,12 @@ function Header() {
         <header className={`${isScrolled && 'bg-[#141414]'}`}>
             {/* left header  */}
             <div className="flex items-center space-x-2 md:space-x-10">
-                <img src="https://rb.gy/ulxxee" width={100} height={100} className="cursor-pointer object-contain" />
+                <img
+                    src="/images/Netflix_2015_logo.svg"
+                    width={100}
+                    height={100}
+                    className="cursor-pointer object-contain"
+                />
 
                 <ul className="hidden space-x-4 md:flex">
                     <li className="header-link">Home</li>
