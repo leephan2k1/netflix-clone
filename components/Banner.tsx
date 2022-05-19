33991923c5812ed2070b606710616cbc38acf419
leephan <pttl2k1@gmail.com>
2022-05-19 21:44:27 +0700

    feat: 🎸 fetch trailer key & genres movie

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,6 +4,8 @@ import { baseUrl } from '~/constants/movie';
 import { Movie } from '~/typings';
 import { FaPlay } from 'react-icons/fa';
 import { InformationCircleIcon } from '@heroicons/react/solid';
+import { useRecoilState } from 'recoil';
+import { modalState, movieState } from '~/atoms/modalAtom';
 
 interface Props {
     netflixOriginals: Array<Movie>;
@@ -11,6 +13,8 @@ interface Props {
 
 function Banner({ netflixOriginals }: Props) {
     const [movie, setMovie] = useState<Movie | null>(null);
+    const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+    const [showModal, setShowModal] = useRecoilState(modalState);
 
     useEffect(() => {
         setMovie(
@@ -44,7 +48,13 @@ function Banner({ netflixOriginals }: Props) {
                 <button className="banner-button bg-white text-black">
                     <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
                 </button>
-                <button className="banner-button bg-[gray]/70">
+                <button
+                    onClick={() => {
+                        setCurrentMovie(movie);
+                        setShowModal(true);
+                    }}
+                    className="banner-button bg-[gray]/70"
+                >
                     More Info{' '}
                     <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
                 </button>
